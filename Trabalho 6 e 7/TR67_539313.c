2b8018cdd26d3aca15f9402d38d617509a797aa9
jpbuenos <pauloxdk9@gmail.com>
2022-12-12 22:32:48 -0300

    demo

---
+++ Trabalho 6 e 7/TR67_539313.c
@@ -0,0 +1,27 @@
+#include "TR67_539313.h"
+
+
+Set *readSet(FILE*)
+{
+	//codigo aqui
+}
+
+Set *emptySet()
+{
+	//codigo aqui
+}
+
+void writeSet(FILE*, Set*);
+{
+	//codigo aqui
+}
+
+void unionSet(Set*, Set*, Set*);
+{
+	//codigo aqui
+} 
+
+void intersecSet(Set*, Set*, Set*);
+{
+	//codigo aqui
+}
